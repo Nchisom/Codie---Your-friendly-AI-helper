11f4370c3b4f615bc0294924cf80253fe9dcb54c
Noah <noahchisom@icloud.com>
2022-12-30 21:09:24 -0800

    updated server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot response
 
-  const response = await fetch("http://localhost:3000", {
+  const response = await fetch("https://codie-m1vq.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
